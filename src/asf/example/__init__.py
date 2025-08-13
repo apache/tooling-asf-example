0ae494f108962e65eea395b4ccca37f0974afc6e
Sean B. Palmer <sean@miscoranda.com>
2025-08-13 20:12:04 +0100

    Add empty tests

--- src/asf/example/__init__.py
+++ src/asf/example/__init__.py
@@ -37,7 +37,7 @@ import tomlkit.items
 
 PROJECT: Final[str] = "asf-example"
 # This is automatically updated
-VERSION: Final[str] = "0.0.1-dev1"
+VERSION: Final[str] = "0.0.1-dev2"
 
 
 class BumpMode(enum.Enum):
