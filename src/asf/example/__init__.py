ebe044a87bceeacdf9cdfffba869f510d1cf79b7
Sean B. Palmer <sean@miscoranda.com>
2025-08-14 19:58:46 +0100

    Change the OIDC JWT audience to atr-test

--- src/asf/example/__init__.py
+++ src/asf/example/__init__.py
@@ -37,7 +37,7 @@ import tomlkit.items
 
 PROJECT: Final[str] = "asf-example"
 # This is automatically updated
-VERSION: Final[str] = "0.0.1-dev4"
+VERSION: Final[str] = "0.0.1-dev5"
 
 
 class BumpMode(enum.Enum):
