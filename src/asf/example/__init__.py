c1428a7cd5bd0d55cfdce9126b4736063454bbca
Sean B. Palmer <sean@miscoranda.com>
2025-08-19 14:40:45 +0100

    Add a script to create a development release

--- src/asf/example/__init__.py
+++ src/asf/example/__init__.py
@@ -37,7 +37,7 @@ import tomlkit.items
 
 PROJECT: Final[str] = "asf-example"
 # This is automatically updated
-VERSION: Final[str] = "0.0.1-dev5"
+VERSION: Final[str] = "0.0.1-dev6"
 
 
 class BumpMode(enum.Enum):
