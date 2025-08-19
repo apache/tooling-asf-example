57912833c7b1b3d032f0abe4fdec0386efac7c4f
Sean B. Palmer <sean@miscoranda.com>
2025-08-19 15:02:53 +0100

    Refer to the repository explicitly when creating a release

--- src/asf/example/__init__.py
+++ src/asf/example/__init__.py
@@ -37,7 +37,7 @@ import tomlkit.items
 
 PROJECT: Final[str] = "asf-example"
 # This is automatically updated
-VERSION: Final[str] = "0.0.1-dev7"
+VERSION: Final[str] = "0.0.1-dev8"
 
 
 class BumpMode(enum.Enum):
