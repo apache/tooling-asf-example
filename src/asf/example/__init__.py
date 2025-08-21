bb339ff62b2c2833d13ebee3ba9709c444080465
Sean B. Palmer <sean@miscoranda.com>
2025-08-21 20:34:52 +0100

    Add a composite action and use it in the ATR upload workflow

--- src/asf/example/__init__.py
+++ src/asf/example/__init__.py
@@ -37,7 +37,7 @@ import tomlkit.items
 
 PROJECT: Final[str] = "asf-example"
 # This is automatically updated
-VERSION: Final[str] = "0.0.1-dev13"
+VERSION: Final[str] = "0.0.1-dev14"
 
 
 class BumpMode(enum.Enum):
