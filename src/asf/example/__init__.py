20b8beaf4b7211af8d7764538c01c4a964f4ef61
Sean B. Palmer <sean@miscoranda.com>
2025-08-22 14:11:57 +0100

    Destructure inputs in the action

--- src/asf/example/__init__.py
+++ src/asf/example/__init__.py
@@ -37,7 +37,7 @@ import tomlkit.items
 
 PROJECT: Final[str] = "asf-example"
 # This is automatically updated
-VERSION: Final[str] = "0.0.1-dev15"
+VERSION: Final[str] = "0.0.1-dev16"
 
 
 class BumpMode(enum.Enum):
