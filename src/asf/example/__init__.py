e77d5065ce88d15ae941ab6ef19ffd9c56943a8d
Sean B. Palmer <sean@miscoranda.com>
2025-08-29 14:30:10 +0100

    Add zizmor to the pre-commit checks

--- src/asf/example/__init__.py
+++ src/asf/example/__init__.py
@@ -37,7 +37,7 @@ import tomlkit.items
 
 PROJECT: Final[str] = "asf-example"
 # This is automatically updated
-VERSION: Final[str] = "0.0.1-dev18"
+VERSION: Final[str] = "0.0.1-dev19"
 
 
 class BumpMode(enum.Enum):
