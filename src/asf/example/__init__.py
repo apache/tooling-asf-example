ad1bf3ddf726f179d3dde7a88782c0299c28652b
Sean B. Palmer <sean@miscoranda.com>
2025-11-20 10:58:14 +0000

    Update Tooling action commit hashes in workflows

--- src/asf/example/__init__.py
+++ src/asf/example/__init__.py
@@ -37,7 +37,7 @@ import tomlkit.items
 
 PROJECT: Final[str] = "asf-example"
 # This is automatically updated
-VERSION: Final[str] = "0.0.1-dev32"
+VERSION: Final[str] = "0.0.1-dev33"
 
 
 class BumpMode(enum.Enum):
